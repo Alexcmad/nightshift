2f3f7a6e50a6dc6181e664238b73cbd5e1898128
Alexander McIntosh <113714949+Alexcmad@users.noreply.github.com>
2025-03-20 18:18:24 -0500

    Initial commit

---
+++ src/test/java/com/alex/nightshift/nightshift/NightshiftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alex.nightshift.nightshift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NightshiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
