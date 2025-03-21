f41481f0977dda9ec70a9b5c85e690959cf76d4c
Alexander McIntosh <113714949+Alexcmad@users.noreply.github.com>
2025-03-20 22:33:56 -0500

    disabled security for now

---
+++ src/main/java/com/alex/nightshift/nightshift/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.alex.nightshift.nightshift.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests()
+                .requestMatchers("/**").permitAll()  // Allow all requests without authentication
+                .and().csrf().disable(); // Optionally disable CSRF if needed
+        return http.build();
+    }
+}
